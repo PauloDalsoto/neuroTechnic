d9bf55d558f46225cd27fa45320013a0fa0c5c34
paulo-dalsoto-isi <153626875+paulo-dalsoto-isi@users.noreply.github.com>
2024-08-15 16:56:48 -0300

    nova rotulagem! e concat

--- functions.py
+++ functions.py
@@ -1,5 +1,16 @@
-def get_data():
-    pass
+def get_data(serial):
+        """Obtém um valor de dados da porta serial."""
+    # return random.uniform(0, 1)
+    try:
+        if serial.in_waiting > 0:
+            data = serial.readline().decode().strip()
+            if data:
+                return float(data)
+            
+    except Exception as e:
+        print(f"Erro ao ler ou converter dados: {e}")
+        serial.close()
+        exit()
 
 def overlap_windows():
     pass
